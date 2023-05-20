f8b146fcac4138259a97f99c7e7e129bbf141e6d
Carlos Henrique Hollerveger <chhollerveger@ciandt.com>
2023-05-20 20:40:02 -0300

    feat(server): added config upload routes

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,28 @@
 import 'dotenv/config'
 import fastify from 'fastify'
+import multipart from '@fastify/multipart'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import fstatic from '@fastify/static'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+app.register(multipart)
 app.register(cors, {
   origin: ['http://localhost:3000'],
 })
 app.register(jwt, {
   secret: 'spacetime',
 })
+app.register(fstatic, {
+  root: resolve(__dirname, '../public/uploads'),
+  prefix: '/uploads',
+})
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
